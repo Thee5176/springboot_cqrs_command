93df4421b197d64055fa8d9173531b8f325c6634
Thee5176 <Theerapong5176@gmail.com>
2025-06-28 14:54:45 +0900

    initialize and setup application.properties

---
+++ src/main/java/com/thee5176/record/springboot_sqrs_command/SpringbootSqrsCommandApplication.java
@@ -0,0 +1,13 @@
+package com.thee5176.record.springboot_sqrs_command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSqrsCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSqrsCommandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thee5176/record/springboot_sqrs_command/SpringbootSqrsCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thee5176.record.springboot_sqrs_command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSqrsCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
