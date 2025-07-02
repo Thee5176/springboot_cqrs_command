d8aa1256de484735b8450d0872c1284354d78295
Thee5176 <Theerapong5176@gmail.com>
2025-07-02 18:49:52 +0900

    refactor project structure

---
+++ src/main/java/com/thee5176/record/springboot_cqrs_command/Application/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.thee5176.record.springboot_cqrs_command.Application.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Service is healthy!");
+    }
+
+}
---
+++ src/main/java/com/thee5176/record/springboot_cqrs_command/Infrastructure/repository/TransactionRepository.java
@@ -0,0 +1,49 @@
+package com.thee5176.record.springboot_cqrs_command.Infrastructure.repository;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.thee5176.record.springboot_cqrs_command.Domain.model.Tables;
+import com.thee5176.record.springboot_cqrs_command.Domain.model.tables.pojos.Transactions;
+
+@Repository
+public class TransactionRepository {
+    
+    @Autowired
+    private DSLContext dslContext;
+
+    //Create - https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/insert-statement/insert-values/#insert-values-with-a-single-row
+    public void createTransaction(Transactions transactions) {
+        dslContext.insertInto(Tables.TRANSACTIONS, Tables.TRANSACTIONS.ID, Tables.TRANSACTIONS.DATE, Tables.TRANSACTIONS.DESCRIPTION,Tables.TRANSACTIONS.CREATED_AT, Tables.TRANSACTIONS.UPDATED_AT)
+            .values(transactions.getId(), transactions.getDate(), transactions.getDescription(), transactions.getCreatedAt(), transactions.getUpdatedAt())
+                .execute();
+    }
+
+    //Update - https://www.jooq.orghttps://www.jooq.org/doc/latest/manual/sql-building/sql-statements/insert-statement/insert-values/#insert-values-with-a-single-row/doc/latest/manual/sql-building/sql-statements/insert-statement/insert-values/#insert-values-with-a-single-row
+    public void updateTransaction(UUID uuid, Transactions Transactions) {
+        dslContext.update(Tables.TRANSACTIONS)
+            .set(Tables.TRANSACTIONS.DATE, Transactions.getDate())
+            .set(Tables.TRANSACTIONS.DESCRIPTION, Transactions.getDescription())
+            .set(Tables.TRANSACTIONS.CREATED_AT, Transactions.getCreatedAt())
+            .set(Tables.TRANSACTIONS.UPDATED_AT, Transactions.getUpdatedAt())
+            .where(Tables.TRANSACTIONS.ID.eq(uuid))
+                .execute();
+    }
+    
+    //Delete - https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/delete-statement/
+    public void deleteTransaction(UUID uuid) {
+        dslContext.delete(Tables.TRANSACTIONS)
+        .where(Tables.TRANSACTIONS.ID.eq(uuid))
+            .execute();
+    }
+
+    //Read - https://www.jooq.org/doc/latest/manual/sql-building/sql-statements/select-statement/#select-from-single-tables
+    public List<Transactions> getTransactions() {
+        return dslContext.selectFrom(Tables.TRANSACTIONS)
+                .fetchInto(Transactions.class);
+    }
+}
