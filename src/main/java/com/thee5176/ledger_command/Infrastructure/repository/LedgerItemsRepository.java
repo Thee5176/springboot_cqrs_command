393fe2d00a6695d3f49491295a1a7c04ce8a65bb
Theerapong Likhitsab <79386713+Thee5176@users.noreply.github.com>
2025-07-21 11:41:11 +0900

    prevent update function to change "CREATED_AT" collumn

--- src/main/java/com/thee5176/ledger_command/Infrastructure/repository/LedgerItemsRepository.java
+++ src/main/java/com/thee5176/ledger_command/Infrastructure/repository/LedgerItemsRepository.java
@@ -26,7 +26,6 @@ public class LedgerItemsRepository {
             .set(Tables.LEDGER_ITEMS.AMOUNT, ledgerItems.getAmount())
             .set(Tables.LEDGER_ITEMS.COA, ledgerItems.getCoa())
             .set(Tables.LEDGER_ITEMS.TYPE, ledgerItems.getType())
-            .set(Tables.LEDGER_ITEMS.CREATED_AT, ledgerItems.getCreatedAt())
             .set(Tables.LEDGER_ITEMS.UPDATED_AT, ledgerItems.getUpdatedAt())
             .where(Tables.LEDGER_ITEMS.ID.eq(uuid))
                 .execute();
