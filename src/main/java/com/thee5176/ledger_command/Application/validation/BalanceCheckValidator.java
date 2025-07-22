ab222f5aa8d86d0d5d19a1ea6e4c40a92ea764e9
Theerapong Likhitsab <79386713+Thee5176@users.noreply.github.com>
2025-07-22 20:04:30 +0900

    Update BalanceCheckValidator.java

--- src/main/java/com/thee5176/ledger_command/Application/validation/BalanceCheckValidator.java
+++ src/main/java/com/thee5176/ledger_command/Application/validation/BalanceCheckValidator.java
@@ -1,6 +1,7 @@
 package com.thee5176.ledger_command.Application.validation;
 
 import java.util.List;
+import java.math.BigDecimal;
 import com.thee5176.ledger_command.Application.dto.CreateLedgerItemsDTO;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -8,14 +9,14 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class BalanceCheckValidator implements ConstraintValidator<BalanceCheck, List<CreateLedgerItemsDTO>> {
     @Override
-	public void initialize(BalanceCheck balanceCheck) {}
+    public void initialize(BalanceCheck balanceCheck) {}
 	
-	@Override
-	public boolean isValid(List<CreateLedgerItemsDTO> ledgerItems, ConstraintValidatorContext context) {
-		return ledgerItems.stream()
-	            	.map(item -> item.getBalance())
-			.map(BigDecimal::new)
-			.reduce(BigDecimal.ZERO, BigDecimal::add)
-			.compareTo(BigDecimal.ZERO) == 0;
-	}
+    @Override
+    public boolean isValid(List<CreateLedgerItemsDTO> ledgerItems, ConstraintValidatorContext context) {
+	return ledgerItems.stream()
+	    .map(item -> item.getBalance())
+	    .map(BigDecimal::new)
+	    .reduce(BigDecimal.ZERO, BigDecimal::add)
+	    .compareTo(BigDecimal.ZERO) == 0;
+}
 }
