2cc88b4c9ad540454ae834c30c0540d0cff25b26
Theerapong Likhitsab <Theerapong5176@gmail.com>
2025-08-27 18:32:44 +0900

    Refactor pom.xml to load database properties from env.properties and update CORS configuration to allow port 80 Remove unused LedgerItemBalanceDTO and accounting settlement service classes

--- src/main/java/com/thee5176/ledger_command/Application/config/WebMvcConfig.java
+++ src/main/java/com/thee5176/ledger_command/Application/config/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173", "http://localhost:8183", "http://" + hostIp + ":8183")
+                    .allowedOrigins("http://localhost:5173", "http://localhost:8183", "http://" + hostIp + ":80")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*");
             }
